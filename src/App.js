c8ffd88d0e2152ae2554f8e9b594974325bf510a
Miguel <miguezaga2@hotmail.com>
2022-12-29 18:58:31 -0500

    add html structure

--- src/App.js
+++ src/App.js
@@ -1,34 +1,64 @@
 import { useState } from "react";
 
 function App() {
+  var [completed, setCompleted] = useState(false);
   return (
     <div>
-        0000 0000 0000 0000
-        Jane Appleseed
-        00/00
+      <main>
+        <div> {/* Cards */}
+          <img /> {/* Card image */}
+          <img /> {/* Card image */}
+          <div>
+            0000 0000 0000 0000
+            Jane Appleseed
+            00/00
+          </div>
 
-        000
+          <div>
+            000
+          </div>
+        </div>
 
-        Cardholder Name
-        e.g. Jane Appleseed
 
-        Card Number
-        e.g. 1234 5678 9123 0000
+      { completed ? (
+        <div>
+          <img /> {/* Confirmation image */}
+            <h2>Thank you!</h2>
+            <p>We've added your card details</p>
+            <button onClick={() => setCompleted(false)}>Continue</button>
+        </div>
+      ) : (
+        <form>
+          <label>
+            Cardholder Name
+            <input type="text" placeholder="e.g. Jane Appleseed" />
+          </label>
 
-        Exp. Date (MM/YY)
-        MM
-        YY
+          <label>
+            Card Number
+            <input type="text" placeholder="e.g. 1234 5678 9123 0000" />
+          </label>
 
-        CVC
-        e.g. 123
+          <label>
+            Exp. Date (MM/YY)
+            <input type="number" placeholder="MM" />
+            <input type="number" placeholder="YY" />
+          </label>
 
-        Confirm
+          <label>
+            CVC
+            <input type="number" placeholder="e.g. 123" />
+          </label>
 
-    {/* Completed state start */}
+          <button onClick={() => setCompleted(true)}type="submit">Confirm</button>
+        </form>
+      )}
 
-        Thank you!
-        We've added your card details
-        Continue
+      </main>
+      <picture>
+        <source />
+        <img /> {/* Background image */}
+      </picture>
     </div>
   );
 }
